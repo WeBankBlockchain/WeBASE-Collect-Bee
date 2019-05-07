3d8e8c75e511f241af7683f9ab9282de7e32d6a4
dalaocu <njumjy06@126.com>
2019-05-07 16:59:26 +0800

    remove client charset

--- src/main/java/com/webank/webasebee/config/MysqlConfig.java
+++ src/main/java/com/webank/webasebee/config/MysqlConfig.java
@@ -27,7 +27,7 @@ import org.hibernate.dialect.MySQL57Dialect;
 public class MysqlConfig extends MySQL57Dialect {
     @Override
     public String getTableTypeString() {
-        return " ENGINE=InnoDB DEFAULT CHARSET=utf8";
+        return " ENGINE=InnoDB DEFAULT CHARSET=utf8mb4";
     }
 
 }
