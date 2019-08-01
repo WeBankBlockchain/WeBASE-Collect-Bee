a5cdb500a5521ccb56c289803046370eb666901b
dalaocu <njumjy06@126.com>
2019-08-01 14:33:05 +0800

    start.sh

--- WeBASE-Collect-Bee-core/src/main/shell/start.sh
+++ WeBASE-Collect-Bee-core/src/main/shell/start.sh
@@ -2,3 +2,8 @@
 
 chmod +x *.jar
 nohup java -jar *.jar >/dev/null 2>&1 &
+echo "Press ctrl + C to break ..."
+echo ""
+echo "Try to start server, please wait."
+sleep 3
+tail -f  webasebee-core.log
