40406555eab8ac8485b968afca65a244583a9e84
dalaocu <njumjy06@126.com>
2019-08-02 17:24:25 +0800

    sh: match dir

--- WeBASE-Collect-Bee-core/src/main/shell/start.sh
+++ WeBASE-Collect-Bee-core/src/main/shell/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 chmod +x *.jar
-nohup java -jar *.jar >/dev/null 2>&1 &
+nohup java -jar `pwd`/*.jar >/dev/null 2>&1 &
 echo "Press ctrl + C to break ..."
 echo ""
 echo "Try to start server, please wait."
